92b2f0aa2af0882ac66e4d3aea1084cfbd549714
Thibert LETULLIER <thibert57@me.com>
2016-11-14 17:41:31 +0100

    polygon shape COR

--- drawing-server/src/shapedrawer/PolygonDrawer.java
+++ drawing-server/src/shapedrawer/PolygonDrawer.java
@@ -1,8 +1,45 @@
 package shapedrawer;
 
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
 /**
  * @author Édouard WILLISSECK
  */
-public class PolygonDrawer
+public class PolygonDrawer extends ShapeDrawerLink
 {
+	
+	public PolygonDrawer(ShapeDrawerLink next) {
+		super(next);
+	}
+
+	@Override
+	public void drawShape(String request, Frame frame, Graphics graphics, BufferStrategy bufferStrategy) throws ShapeNotRecognizedException {
+		
+		String[] arguments = request.split(",");
+		String shape = arguments[0];
+		
+		if(!shape.toLowerCase().equals("triangle") && !shape.toLowerCase().equals("polygon"))
+		{
+			throw new ShapeNotRecognizedException("Shape not recognized");
+		}
+		
+        String color = arguments[1];
+        
+        int nbPoint = (arguments.length-2)/2;
+        
+        int[] pX = null, pY = null;
+        
+        for (int i = 0; i < nbPoint; i++)
+        {
+        	pX[i] = Integer.parseInt(arguments[2+i*2]);
+        	pY[i] = Integer.parseInt(arguments[3+i*2]);
+        }
+        
+        //TODO get color 
+        graphics.fillPolygon(pX, pY, nbPoint);
+        
+	}
+	
 }
