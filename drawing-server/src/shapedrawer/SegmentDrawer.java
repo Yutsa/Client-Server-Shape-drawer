05c5716fdce6b32019b5798848831f64f8985c77
Prosor <nicolasmartin96@gmail.com>
2016-11-14 17:09:58 +0100

    Continue the SegmentDrawer class

--- drawing-server/src/shapedrawer/SegmentDrawer.java
+++ drawing-server/src/shapedrawer/SegmentDrawer.java
@@ -1,8 +1,27 @@
 package shapedrawer;
+import java.awt.*;
 
-/**
- * @author Édouard WILLISSECK
- */
-public class SegmentDrawer
+public class SegmentDrawer extends ShapeDrawerLink
 {
+    public SegmentDrawer(ShapeDrawerLink next)
+    {
+        super(next);
+    }
+    public void drawShape(String request, Frame frame) throws ShapeNotRecognizedException
+    {
+        String[] arguments = request.split(",");
+        String shape = arguments[0];
+        if(!shape.toLowerCase().equals("segment")){
+            throw new ShapeNotRecognizedException("Shape Not Recognized")
+        }
+        String color = arguments[1];
+        int coordXA = Integer.parseInt(arguments[2]);
+        int coordYA = Integer.parseInt(arguments[3]);
+        int coordXB = Integer.parseInt(arguments[4]);
+        int coordYB = Integer.parseInt(arguments[5]);
+
+        Graphics graphics = getGraphics(frame);
+        graphics.drawLine(coordXA,coordYA,coordXB,coordYB);
+
+    }
 }
