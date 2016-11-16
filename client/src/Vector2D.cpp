00d49dbc81297df90f394c2e2a132107d57338bc
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-11-16 17:00:13 +0100

    Fixed errors in Vector2D.hpp

---
+++ client/src/Vector2D.cpp
@@ -0,0 +1,18 @@
+#include "Vector2D.hpp"
+
+Vector2D::Vector2D(double x, double y)
+{
+    //TODO: Check if x and y can be < 0 or not.
+    _x = x;
+    _y = y;
+}
+
+Vector2D::~Vector2D()
+{
+    // Destructor
+}
+
+const Vector2D & add(const Vector2D & lhv, const Vector2D & rhv)
+{
+    return Vector2D(lhv.getX() + rhv.getX(), lhv.getY() + rhv.getY());
+}
--- client/src/Vector2D.hpp
+++ client/src/Vector2D.hpp
@@ -1,13 +1,25 @@
 #include <iostream>
 
+using std::string;
+
 class Vector2D
 {
 private:
     double _x;
     double _y;
+    const Vector2D & add(const Vector2D & lhv,
+        const Vector2D & rhv);
+    const Vector2D & multiply(const Vector2D & lhv,
+        const Vector2D & rhv);
+    const Vector2D & substract(const Vector2D & lhv,
+        const Vector2D & rhv);
 public:
     Vector2D(double x, double y);
     virtual ~Vector2D();
+    double getX() const { return _x;}
+    double getY() const { return _y;}
+
+    // Operators
     operator string() const;
     friend const Vector2D operator+ (const Vector2D & lhv,
         const Vector2D & rhv);
@@ -15,13 +27,7 @@ public:
         const Vector2D & rhv);
     friend const Vector2D operator- (const Vector2D & lhv,
         const Vector2D & rhv);
-    const Vector2D & add(const Vector2D & lhv,
-        const Vector2D & rhv);
-    const Vector2D & multiply(const Vector2D & lhv,
-        const Vector2D & rhv);
-    const Vector2D & substract(const Vector2D & lhv,
-        const Vector2D & rhv);
-}
+};
 
 extern const Vector2D operator+ (const Vector2D & lhv,
     const Vector2D & rhv);
