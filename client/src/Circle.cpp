97bcbac1f085ce77c30e834e784991766c4ffb4c
Prosor <nicolasmartin96@gmail.com>
2016-11-17 15:01:43 +0100

    Add the Circle.cpp file

---
+++ client/src/Circle.cpp
@@ -0,0 +1,41 @@
+#include "Circle.hpp"
+
+Circle(double x, double y, double diameter)
+{
+
+}
+
+virtual void draw(const DrawingVisitor* visitor) const
+{
+  
+}
+
+virtual operator string() const
+{
+
+}
+
+virtual void save(const SaveVisitor* saveVisitor) const
+{
+
+}
+
+virtual void translation()
+{
+
+}
+
+virtual void homothety()
+{
+
+}
+
+virtual void rotation()
+{
+
+}
+
+virtual double getArea() const
+{
+
+}
