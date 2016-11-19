34fa1458aa80a4b3d47d8b24ee0823c3c688e6a0
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-11-19 17:35:35 +0100

    SaveTextVisitor Working ./../bin/test-saving !

--- client/src/Tests/test-saving.cpp
+++ client/src/Tests/test-saving.cpp
@@ -1,8 +1,9 @@
-
-
-/* getenv example: getting path */
 #include <cstring>
 #include <iostream>
+#include "../Shape/Circle.hpp"
+#include "../Utils/Color.hpp"
+#include "../Visitor/SaveTextVisitor.hpp"
+//#include "../Visitor/SaveVisitor.hpp"
 
 using std::cout;
 using std::endl;
@@ -10,14 +11,10 @@ using std::string;
 
 int main ()
 {
-    char* pPath;
-    string prout = "prout";
-    pPath = getenv ("HOME");
-    if (pPath!=NULL)
-    {
-        strcat(pPath, "/");
-        strcat(pPath, prout.c_str());
-        cout << pPath << endl;
-    }
-    return 0;
+    SaveTextVisitor* savior = new SaveTextVisitor();
+    Vector2D center = Vector2D(10, 10);
+    Color color = Color::getColor("blue");
+    Circle prout = Circle(center, 30, color);
+    const string filename = "PROUT";
+    prout.save(savior, filename);
 }
--- client/src/Visitor/SaveTextVisitor.cpp
+++ client/src/Visitor/SaveTextVisitor.cpp
@@ -1,18 +1,89 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
 #include "SaveTextVisitor.hpp"
+#include "../Shape/Circle.hpp"
+#include "../Shape/Triangle.hpp"
+#include "../Shape/Segment.hpp"
+#include "../Shape/Polygon.hpp"
 
 using std::string;
+using std::ofstream;
 
 void SaveTextVisitor::save(const Circle *circle,
     const string & filename) const
 {
     char* path;
+    ofstream file;
     string textShape = (string) *circle;
     path = getenv("HOME");
     if (path != NULL)
     {
-        strcat(pPath, "/");
+        strcat(path, "/");
         strcat(path, filename.c_str());
+        file.open(path);
+        file << textShape << "\n";
+        file.close();
     }
+}
+
+void SaveTextVisitor::save(const Segment *segment,
+    const string & filename) const
+{
+    char* path;
+    ofstream file;
+    string textShape = (string) *segment;
+    path = getenv("HOME");
+    if (path != NULL)
+    {
+        strcat(path, "/");
+        strcat(path, filename.c_str());
+        file.open(path);
+        file << textShape << "\n";
+        file.close();
+    }
+}
+
+void SaveTextVisitor::save(const Triangle *triangle,
+    const string & filename) const
+{
+    char* path;
+    ofstream file;
+    string textShape = (string) *triangle;
+    path = getenv("HOME");
+    if (path != NULL)
+    {
+        strcat(path, "/");
+        strcat(path, filename.c_str());
+        file.open(path);
+        file << textShape << "\n";
+        file.close();
+    }
+}
+
+void SaveTextVisitor::save(const Polygon *polygon,
+    const string & filename) const
+{
+    char* path;
+    ofstream file;
+    string textShape = (string) *polygon;
+    path = getenv("HOME");
+    if (path != NULL)
+    {
+        strcat(path, "/");
+        strcat(path, filename.c_str());
+        file.open(path);
+        file << textShape << "\n";
+        file.close();
+    }
+}
+
+SaveTextVisitor::SaveTextVisitor() : SaveVisitor()
+{
+
+}
+
+SaveTextVisitor::~SaveTextVisitor()
+{
+
 }
