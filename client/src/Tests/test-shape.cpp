17830630066986a1beb52e6e95b81dab70bc4f15
Prosor <nicolasmartin96@gmail.com>
2016-11-19 17:38:50 +0100

    Finish all test of shapes and everything is working

--- client/src/Tests/test-shape.cpp
+++ client/src/Tests/test-shape.cpp
@@ -23,28 +23,102 @@ int main(int argc, char* argv[])
 
     cout << "===== Circle =====" << endl << endl;
 
-    Vector2D center = Vector2D(10, 10);
+        Vector2D center = Vector2D(10, 10);
 
-    Circle C = Circle(center, 30, red);
-    Shape* CModif;
+        Circle C = Circle(center, 30, red);
+        Shape* CModif;
 
-    cout << "Normal : " << C << " Aire : " << C.getArea() << endl;
+        cout << "Normal : " << C << " Aire : " << C.getArea() << endl;
 
-    CModif = C.homothety(invariantPoint, homothetyRatio);
-    cout << "Homothety : "  << *CModif << " Aire : " << C.getArea() << endl;
+        CModif = C.homothety(invariantPoint, homothetyRatio);
+        cout << "Homothety : "  << *CModif << " Aire : " << CModif->getArea() << endl;
 
-    CModif = C.translation(translationVector);
-    cout << "Translation : " << *CModif << " Aire : " << C.getArea() << endl;
+        CModif = C.translation(translationVector);
+        cout << "Translation : " << *CModif << " Aire : " << CModif->getArea() << endl;
 
-    CModif = C.rotation(rotationCenter, rotationAngle);
-    cout << "Rotation : " << *CModif << " Aire : " << C.getArea() << endl;
+        CModif = C.rotation(rotationCenter, rotationAngle);
+        cout << "Rotation : " << *CModif << " Aire : " << CModif->getArea() << endl << endl;
 
     cout << "===== Polygon =====" << endl << endl;
 
+        Shape* PModif;
+
+        Polygon P = Polygon(red);
+        Vector2D P1P = Vector2D(10,10);
+        P.addPoint(P1P);
+        Vector2D P2P = Vector2D(20,10);
+        P.addPoint(P2P);
+        Vector2D P3P = Vector2D(20,20);
+        P.addPoint(P3P);
+        Vector2D P4P = Vector2D(10,20);
+        P.addPoint(P4P);
+
+        cout << "       Polygone convexe        " << endl;
+
+        cout << "Normal : " << P << " Aire : " << P.getArea() << endl;
+
+        PModif = P.homothety(invariantPoint, homothetyRatio);
+        cout << "Homothety : "  << *PModif << " Aire : " << PModif->getArea() << endl;
+
+        PModif = P.translation(translationVector);
+        cout << "Translation : " << *PModif << " Aire : " << PModif->getArea() << endl;
+
+        PModif = P.rotation(rotationCenter, rotationAngle);
+        cout << "Rotation : " << *PModif << " Aire : " << PModif->getArea() << endl << endl;
+
+        Vector2D P5P = Vector2D(15,15);
+        P.addPoint(P5P);
+
+        cout << "       Polygone concave        " << endl;
+
+        cout << "Normal : " << P << " Aire : " << P.getArea() << endl;
+
+        PModif = P.homothety(invariantPoint, homothetyRatio);
+        cout << "Homothety : "  << *PModif << " Aire : " << PModif->getArea() << endl;
+
+        PModif = P.translation(translationVector);
+        cout << "Translation : " << *PModif << " Aire : " << PModif->getArea() << endl;
+
+        PModif = P.rotation(rotationCenter, rotationAngle);
+        cout << "Rotation : " << *PModif << " Aire : " << PModif->getArea() << endl << endl;
+
+
     cout << "===== Segment =====" << endl << endl;
+
+        Shape* SModif;
+        Vector2D P1S = Vector2D(10,10);
+        Vector2D P2S = Vector2D(20,10);
+        Segment S = Segment(P1S,P2S,red);
+
+        cout << "Normal : " << S << " Aire : " << S.getArea() << endl;
+
+        SModif = S.homothety(invariantPoint, homothetyRatio);
+        cout << "Homothety : "  << *SModif << " Aire : " << SModif->getArea() << endl;
+
+        SModif = S.translation(translationVector);
+        cout << "Translation : " << *SModif << " Aire : " << SModif->getArea() << endl;
+
+        SModif = S.rotation(rotationCenter, rotationAngle);
+        cout << "Rotation : " << *SModif << " Aire : " << SModif->getArea() << endl << endl;
 
     cout << "===== Triangle =====" << endl << endl;
 
+        Shape* TModif;
+        Vector2D P1T = Vector2D(10,10);
+        Vector2D P2T = Vector2D(20,10);
+        Vector2D P3T = Vector2D(20,20);
+        Triangle T = Triangle(P1T,P2T,P3T,red);
+
+        cout << "Normal : " << T << " Aire : " << T.getArea() << endl;
+
+        TModif = T.homothety(invariantPoint, homothetyRatio);
+        cout << "Homothety : "  << *TModif << " Aire : " << TModif->getArea() << endl;
+
+        TModif = T.translation(translationVector);
+        cout << "Translation : " << *TModif << " Aire : " << TModif->getArea() << endl;
+
+        TModif = T.rotation(rotationCenter, rotationAngle);
+        cout << "Rotation : " << *TModif << " Aire : " << TModif->getArea() << endl << endl;
 
 
 
