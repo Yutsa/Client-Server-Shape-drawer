fc95b1926e1682855f9007cde6a4dd10cc78efe2
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-11-25 17:28:03 +0100

    Found why QtDrawer is considered const.
    * Added a commentary to explain why the instance of QtDrawer is
      const. It has to be fixed.
    
    * Still has to find a way to get each shapes from the ComposedShape
      and pass it to the addToScene method. As of now we get a Shape*
      from the getShape() method and passing a Shape* to the addToScene
      method is not working. It expects a Circle*, Triangle*, Segment*
      or Polygon*.

--- client/src/Tests/test-qtdrawer.cpp
+++ client/src/Tests/test-qtdrawer.cpp
@@ -13,6 +13,15 @@ using std::cout;
 using std::endl;
 using std::string;
 
+
+/*
+ * QtDrawer is considered as const because when it calls its
+ * draw method inside the Shape's draw method, it's passed 
+ * as a "const DrawingVisitor &" so it is const.
+ * It can't be anymore since QtDrawer modifies its instance when
+ * drawing (the scene, the view and so on.)
+ */
+
 int main(int argc, char* argv[])
 {
     QtDrawer *qtDrawer = new QtDrawer();
@@ -20,6 +29,7 @@ int main(int argc, char* argv[])
     Vector2D center(10, 50);
     Circle C(center, 400, Color::getColor("red"));
     C.draw(qtDrawer);
+
     Polygon P = Polygon(Color::getColor("red"));
     Vector2D P1P = Vector2D(250,250);
     P.addPoint(P1P);
