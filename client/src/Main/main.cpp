d4b8da656df6a599f6d2d854bbddd3ba1f80ab33
Édouard WILLISSECK <edouard.willisseck@gmail.com>
2016-12-08 18:58:16 +0100

    Updated main

--- client/src/Main/main.cpp
+++ client/src/Main/main.cpp
@@ -17,7 +17,6 @@ using std::string;
 
 int main(int argc, char* argv[])
 {
-/**
     //=============== Variables Init ===============//
 
     Vector2D center(50, 50);
@@ -45,72 +44,101 @@ int main(int argc, char* argv[])
     P1->addPoint(point7);
     P1->addPoint(point4);
     Shape* P = P1;
-**/
+
 //=============== Start Connexion ===============//
 
     ServerDrawer *serverDrawer = new ServerDrawer();
     QtDrawer *qtDrawer = new QtDrawer();
-/**
+
 //=============== Draw Circles ===============//
 
+    getchar();
     C->draw(serverDrawer);
     C->draw(qtDrawer);
 
-    C->rotation(centerRotation, pi);
-    C->draw(serverDrawer);
 
+    cout << "Press Enter to zoom on circle." << endl;
+    getchar();
     C = C->homothety(center, ratio);
     C->draw(serverDrawer);
 
+    cout << "Press Enter to do a translation on the circle." << endl;
+    getchar();
     C = C->translation(vectorTranslation);
     C->draw(serverDrawer);
 
 //=============== Draw Segment ===============//
 
+    cout << "Press Enter to draw a segment" << endl;
+    getchar();
     S->draw(serverDrawer);
     S->draw(qtDrawer);
 
+    cout << "Press Enter to rotate the segment" << endl;
+    getchar();
     S = S->rotation(centerRotation2, pi/2);
     S->draw(serverDrawer);
 
+    cout << "Press Enter to zoom on the segment" << endl;
+    getchar();
     S = S->homothety(centerRotation2, ratio2);
     S->draw(serverDrawer);
 
+    cout << "Press Enter to do a translation on the segment" << endl;
+    getchar();    
     S = S->translation(vectorTranslation);
     S->draw(serverDrawer);
 
 //=============== Draw Triangle ===============//
 
+    cout << "Press Enter to draw a triangle" << endl;
+    getchar();
     T->draw(serverDrawer);
     T->draw(qtDrawer);
 
+    cout << "Press Enter to do a rotation on the triangle" << endl;
+    getchar();        
     T = T->rotation(centerRotation3, pi/2);
     T->draw(serverDrawer);
 
+    cout << "Press Enter to do a translation on the triangle" << endl;
+    getchar();            
     T = T->translation(vectorTranslation);
     T->draw(serverDrawer);
 
-    T = T->homothety(point1, ratio);
+    cout << "Press Enter to do a zoom on the triangle" << endl;
+    getchar();
+    T = T->homothety(point1, 0.5);
     T->draw(serverDrawer);
 
 //=============== Draw Polygon ===============//
 
+    cout << "Press Enter to draw a polygon" << endl;
+    getchar();    
     P->draw(serverDrawer);
     P->draw(qtDrawer);
 
+    cout << "Press Enter to do a translation on the polygon" << endl;
+    getchar();
     P = P->translation(vectorTranslation2);
     P->draw(serverDrawer);
 
-    P = P->homothety(point1, ratio);
+    cout << "Press Enter to do a zoom on the polygon" << endl;
+    getchar();
+    P = P->homothety(point1, 0.3);
     P->draw(serverDrawer);
 
+    cout << "Press Enter to do a rotation on the polygon" << endl;
+    getchar();
     P = P->rotation(point3, pi/4);
     P->draw(serverDrawer);
 
-**/
+
 
 //=============== Draw Compsed Shape ===============//
 
+    cout << "Press Enter to do draw the composed shape" << endl;
+    getchar();    
     Vector2D centerCP(50, 100);
     Vector2D beginSegCP(300, 100), endSegCP(400, 150);
     Vector2D point1CP(450, 200), point2CP(500, 270), point3CP(500, 200);
--- questions_prof.md
+++
@@ -1,6 +0,0 @@
-Serveur bloquant
-=================
-
-On a besoin de fermer le client pour que ça dessine ou que ça affiche.
-
-Il doit y avoir un truc bloquant dans le serveur.
